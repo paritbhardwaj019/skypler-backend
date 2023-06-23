27dadbb89bc46cd34b2e8e2f16a19dad90bac704
Parit Bhardwaj <paritbhardwaj@outlook.com>
2023-06-23 18:38:30 +0530

    feature/charts

--- controllers/expenses_controller.js
+++ controllers/expenses_controller.js
@@ -21,4 +21,22 @@ module.exports = {
         .json({ message: "Internal Server Error", success: false });
     }
   },
+  fetchAllExpenses: async (req, res) => {
+    try {
+      const expenses = await expensesService.getAllExpense();
+
+      res.status(200).json({
+        success: true,
+        length: expenses.length,
+        data: {
+          expenses,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      res
+        .status(500)
+        .json({ message: "Internal Server Error", success: false });
+    }
+  },
 };
--- routes/expenses_route.js
+++ routes/expenses_route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const expensesController = require("../controllers/expenses_controller");
 
 router.get("/", expensesController.fetchExpensesByProjectId);
+router.get("/all", expensesController.fetchAllExpenses);
 
 module.exports = router;
